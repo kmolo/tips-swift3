326a0464df1e0cf58af3cc29004a8c64f3e02abb
kmolo <kmoloney@mail.ccsf.edu>
2016-12-07 15:55:00 -0800

    Initial Commit

---
+++ tips-swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips-swift3
+//
+//  Created by mny on 12/7/16.
+//  Copyright © 2016 ccsf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
