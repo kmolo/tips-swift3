e0aa5b9988c8f4bfe867077deed4e1e3811aa00b
kmolo <kmoloney@mail.ccsf.edu>
2017-01-04 05:23:58 -0800

    create IBOutLet for UI elements
    - UITextField: billField
    - UILabel: tipLabel, totalLabel
    - UISegmentedControl: tipControl

--- tips-swift3/ViewController.swift
+++ tips-swift3/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
